b5c4358722292bf7f3ef4a50f8e8985f7a32d157
Askar <osbelkz@gmail.com>
2020-10-11 12:32:29 +0600

    gh-pages

--- src/n1-main/m1-ui/Main/Main.tsx
+++ src/n1-main/m1-ui/Main/Main.tsx
@@ -10,7 +10,7 @@ import NewPasswordContainer from "../../../n2-features/f1-auth/a4-new-password/N
 import {Button} from "../common/Button/Button";
 import {Input} from "../common/Input/Input";
 
-const Main = () => {
+const Main: React.FC = () => {
     return (
         <div className={classes.main}>
             <Header/>
@@ -28,25 +28,43 @@ const Main = () => {
                 <div>
                     <div>
                         <h3>Regular</h3>
-                        <Button onClick={() => alert("clicked")} btnName={"Button"}/>
-                        <Button onClick={() => alert("clicked")} btnName={"Button"} disabled={true}/>
+                        <Button onClick={() => alert("clicked")}
+                                btnName={"Button"}/>
+                        <Button onClick={() => alert("clicked")}
+                                btnName={"Button"}
+                                disabled={true}/>
                     </div>
                     <div>
                         <h3>Green</h3>
-                        <Button onClick={() => alert("clicked")} btnName={"Button"} btnType={"green"}/>
-                        <Button onClick={() => alert("clicked")} btnName={"Button"} btnType={"green"} disabled={true}/>
+                        <Button onClick={() => alert("clicked")}
+                                btnName={"Button"}
+                                btnType={"green"}/>
+                        <Button onClick={() => alert("clicked")}
+                                btnName={"Button"}
+                                btnType={"green"}
+                                disabled={true}/>
                     </div>
                     <div>
                         <h3>Red</h3>
-                        <Button onClick={() => alert("clicked")} btnName={"Button"} btnType={"red"}/>
-                        <Button onClick={() => alert("clicked")} btnName={"Button"} btnType={"red"} disabled={true}/>
+                        <Button onClick={() => alert("clicked")}
+                                btnName={"Button"}
+                                btnType={"red"}/>
+                        <Button onClick={() => alert("clicked")}
+                                btnName={"Button"}
+                                btnType={"red"}
+                                disabled={true}/>
                     </div>
                 </div>
                 <br/>
                 <div>
                     <h3>Input</h3>
-                    <Input onChange={() => alert("was changed")} value={"sadf"} placeHolder={"input"}/>
-                    <Input onChange={() => alert("was changed")} value={""} placeHolder={"input"} disabled={true}/>
+                    <Input onChange={() => alert("was changed")}
+                           value={"sadf"}
+                           placeHolder={"input"}/>
+                    <Input onChange={() => alert("was changed")}
+                           value={""}
+                           placeHolder={"input"}
+                           disabled={true}/>
                 </div>
             </div>
         </div>
