5f5ce138f78a628cdeaad61c19b6f5bba03b6e0a
Constantin-dot <konstantinkspb@gmail.com>
2020-10-28 18:43:32 +0300

    added universal slider, added universal search with input and double range

--- src/n1-main/m1-ui/common/Search/Search.tsx
+++ src/n1-main/m1-ui/common/Search/Search.tsx
@@ -2,26 +2,48 @@ import React, {ChangeEvent, useState} from "react";
 import classes from "./Search.module.scss";
 import {Input} from "../Input/Input";
 import {Button} from "../Button/Button";
+import {Slider} from "../Slider/Slider";
 
 type SearchPropsType = {
+    name: string | undefined
     label: string
+    minValue: number
+    maxValue: number
+    stepValue: number
+    setSearchParams: (searchName?: string, min?: number, max?: number) => void
 }
 
 export const Search = (props: SearchPropsType) => {
-    const [name, setName] = useState("")
-    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const [name, setName] = useState(props.name)
+    const [min, setMin] = useState(0)
+    const [max, setMax] = useState(150)
+    const inputOnChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setName(e.currentTarget.value)
     }
 
+
     return <div className={classes.uniSearch}>
+        <h3>{props.label}</h3>
         <Input
-            label={props.label}
-            onChange={onChangeHandler}
+            value={name}
+            onChange={inputOnChangeHandler}
         />
-        <Button
-            btnName={"Search"}
-            btnType={"green"}
-            onClick={() => {alert(name)}}
-        />
+        <div className={classes.slider}>
+            <Slider
+                setMin={setMin}
+                setMax={setMax}
+                min={min}
+                max={max}
+                minValue={props.minValue}
+                maxValue={props.maxValue}
+                stepValue={props.stepValue}
+            />
+        </div>
+            <Button
+                btnName={"Search"}
+                onClick={() => {
+                    props.setSearchParams(name, min, max)
+                }}
+            />
     </div>
 }
